e476cbbe2c39831bf5e6cc99e3fbb640ea65b39c
KingKacrae99 <kacrae9@gmail.com>
2025-04-29 09:49:46 +0100

    API View Written

--- core/views/staff_views.py
+++ core/views/staff_views.py
@@ -1,1 +1,15 @@
-'__all__' = []
+__all__ = []
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from core.models import Staff
+from core.serializers import StaffSerializer
+
+@api_view(['GET'])
+def get_staff_list(request):
+    if request.method == 'GET':
+        rides = Staff.objects.all()
+        serializer = StaffSerializer(rides, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({'error': 'Invalid request method'}, status=status.HTTP_400_BAD_REQUEST)
--- core/views/user_views.py
+++ core/views/user_views.py
@@ -1,1 +1,23 @@
-'__all__' = []
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from core.models import CustomUser
+from core.serializers import CustomUserSerializer
+
+@api_view(['GET','POST'])
+def get_user_list(request):
+    if request.method == 'GET':
+        users = CustomUser.objects.all()
+        serializer = CustomUserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+@api_view(['POST'])
+def create_user(request):
+    if request.method == 'POST':
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+__all__ = ['get_user_list', 'create_user']
